85b688927ac02345740f72a354f6372fe0cdae36
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-23 12:17:16 -0500

    Update TestRegularStruct.sol

--- TestRegularStruct.sol
+++ TestRegularStruct.sol
@@ -5,9 +5,9 @@ import "./RegularStruct.sol";
 contract TestRegularStruct is RegularStruct {
 
   function test_setStruct() {
-      setStruct(1,2,3,4,5);
-      setStruct(6, 8, 0, 200);
-      setStruct(105, 83, 24, 12);
-      setStruct(50, 50, 50, 50);
+      setStruct(1, 2, 3, 4, 5);
+      setStruct(6, 8, 0, 200, 225);
+      setStruct(105, 83, 24, 12, 14);
+      setStruct(50, 50, 50, 50, 50);
   }
 }
